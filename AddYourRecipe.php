190eae214ffadcbc7f5bc1a7446448b5ced961b0
RM-Ib <ranimibrahim145@gmail.com>
2025-04-20 12:17:14 +0300

    Update AddYourRecipe.php

--- AddYourRecipe.php
+++ AddYourRecipe.php
@@ -2,7 +2,9 @@
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-
+echo '<pre>Session Contents: ';
+print_r($_SESSION);
+echo '</pre>';
 // Check if user is not logged in
 if (!isset($_SESSION['user_id'])) {
     $_SESSION['error'] = "You must login first.";
@@ -13,6 +15,7 @@ if (!isset($_SESSION['user_id'])) {
 
 // If logged in, continue showing the page
 ?>
+
 
 <!DOCTYPE html>
 <!-- page created by Mira Kamal + navbar + footer -->
